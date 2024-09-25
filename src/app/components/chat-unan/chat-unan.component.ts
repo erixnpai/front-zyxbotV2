6026273e0ca9f73d7cab3b48005ab2b72a11e489
erinxpai <erianhernandez97@gmail.com>
2024-09-25 14:33:51 -0600

    chat unan responsive

--- src/app/components/chat-unan/chat-unan.component.ts
+++ src/app/components/chat-unan/chat-unan.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { toast } from 'ngx-sonner';
+import { AuthStateService } from '../../services/data-access/auth-state.service';
+import { LoginService } from '../../services/login/auth.service';
 
 @Component({
   selector: 'app-chat-unan',
@@ -10,5 +13,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './chat-unan.component.css'
 })
 export default class ChatUnanComponent {
+  userPhotoURL: string | null = null;
+  user: string | null = null
+  private _auth = inject(AuthStateService);
+  
+  constructor(public srvAuth: LoginService, private router: Router) {
+    this.CargardatoUser()
+  }
+  async CargardatoUser() {
+    try {
+      this.userPhotoURL = this.srvAuth.getUserPhotoURL();
+      this.user = this.srvAuth.getUser();
+      // await this.srvAuth.getUserPhotoURL();
+      // toast.success('carga en usuario');
 
+      // Obtén la URL de la foto del usuario
+
+      console.log("Imagen del usuario", this.userPhotoURL);
+      // console.log("Imagen del usuario", this.user);
+      // this.router.navigateByUrl('/');
+    } catch (error) {
+      toast.error('Error al cargar usuario con Google');
+    }
+  }
 }
